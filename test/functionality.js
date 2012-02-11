786e6bf5125cddacfa592b6b3cd53229c2417299
Christopher Mooney <chris@dod.net>
2012-02-10 18:33:26 -0800

    removing old submodules.

--- test/functionality.js
+++ test/functionality.js
@@ -1,33 +1,10 @@
-var testCase = require('nodeunit').testCase;
+exports.testSomething = function(test){
+    test.expect(1);
+    test.ok(true, "this assertion should pass");
+    test.done();
+};
 
-module.exports.bar = testCase({
-    setUp: function (callback) {
-        this.foo = 'bar';
-        callback();
-    },
-    tearDown: function (callback) {
-        // clean up
-        callback();
-    },
-    test1: function (test) {
-        test.expect(1);
-        test.equals(this.foo, 'bar');
-        test.done();
-    }
-});
-
-module.exports.baz = testCase({
-    setUp: function (callback) {
-        this.foo = 'baz';
-        callback();
-    },
-    tearDown: function (callback) {
-        // clean up
-        callback();
-    },
-    test1: function (test) {
-        test.expect(1);
-        test.equals(this.foo, 'baz');
-        test.done();
-    }
-});
+exports.testSomethingElse = function(test){
+    test.ok(true, "this assertion should fail");
+    test.done();
+};
