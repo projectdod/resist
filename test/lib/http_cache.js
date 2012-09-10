4339e9012d199efd6955cde65d7c6569f9fd5ec7
Christopher Mooney <chris@dod.net>
2012-09-10 11:25:47 -0700

    Weekend work on tests.  More complete.

--- test/lib/http_cache.js
+++ test/lib/http_cache.js
@@ -220,7 +220,7 @@ exports.http_cache = {
     test.expect(3);
     test.isNotNull(this.httpCache.getBody);
     test.isBuffer(this.httpCache.getBody());
-    test.equal(this.httpCache.getBody().toString(), "");
+    test.equal(this.httpCache.getBody().length, 0);
     test.done();
   },
   'should have setBody method' : function (test) {
@@ -229,10 +229,32 @@ exports.http_cache = {
     test.isFunction(this.httpCache.setBody);
     test.done();
   },
+  'setBody should actually set value in object' : function (test) {
+    test.expect(6);
+    test.isNotNull(this.httpCache.getBody);
+    test.isNotNull(this.httpCache.setBody);
+    test.isBuffer(this.httpCache.getBody());
+    test.equal(this.httpCache.getBody().length, 0);
+    var test1 = new Buffer("test1");
+    this.httpCache.setBody(test1);
+    test.equal(this.httpCache.getBody().toString(), test1.toString());
+    var test2 = new Buffer("test2");
+    this.httpCache.setBody(test2);
+    test.equal(this.httpCache.getBody().toString(),
+      test1.toString() + test2.toString());
+    test.done();
+  },
   'should have getKeyPrefix method' : function (test) {
     test.expect(2);
     test.isNotNull(this.httpCache.getKeyPrefix);
     test.isFunction(this.httpCache.getKeyPrefix);
+    test.done();
+  },
+  'getKeyPrefix will be getVersionPrefix if not set' : function (test) {
+    test.expect(2);
+    test.isNotNull(this.httpCache.getKeyPrefix);
+    test.equal(this.httpCache.getKeyPrefix(),
+      this.httpCache.getVersionPrefix());
     test.done();
   },
   'should have setKeyPrefix method' : function (test) {
@@ -241,10 +263,27 @@ exports.http_cache = {
     test.isFunction(this.httpCache.setKeyPrefix);
     test.done();
   },
+  'setKeyPrefix should actually set value in object' : function (test) {
+    test.expect(5);
+    test.isNotNull(this.httpCache.getVersionPrefix);
+    test.isNotNull(this.httpCache.getKeyPrefix);
+    test.isNotNull(this.httpCache.setKeyPrefix);
+    test.equal(this.httpCache.getKeyPrefix(),
+      this.httpCache.getVersionPrefix());
+    this.httpCache.setKeyPrefix("test");
+    test.equal(this.httpCache.getKeyPrefix(), "test");
+    test.done();
+  },
   'should have getRequest method' : function (test) {
     test.expect(2);
     test.isNotNull(this.httpCache.getRequest);
     test.isFunction(this.httpCache.getRequest);
+    test.done();
+  },
+  'getRequest will be undef if not set' : function (test) {
+    test.expect(2);
+    test.isNotNull(this.httpCache.getRequest);
+    test.isUndefined(this.httpCache.getRequest());
     test.done();
   },
   'should have setRequest method' : function (test) {
@@ -253,16 +292,57 @@ exports.http_cache = {
     test.isFunction(this.httpCache.setRequest);
     test.done();
   },
+  'setRequest should actually set value in object' : function (test) {
+    test.expect(10);
+    test.isNotNull(this.httpCache.getKeyPrefix);
+    test.isNotNull(this.httpCache.getVersionPrefix);
+    test.isNotNull(this.httpCache.getMobilePrefix);
+    test.isNotNull(this.httpCache.getRequest);
+    test.isNotNull(this.httpCache.setRequest);
+    test.isUndefined(this.httpCache.getRequest());
+    var testObject = new Object();
+    testObject.headers = {
+      "user-agent" : "testing"
+    };
+    this.httpCache.setRequest(testObject);
+    test.equal(this.httpCache.getRequest(), testObject);
+    test.equal(this.httpCache.getKeyPrefix(),
+      this.httpCache.getVersionPrefix());
+    testObject.headers = {
+      "user-agent" : "iPhone"
+    };
+    this.httpCache.setRequest(testObject);
+    test.equal(this.httpCache.getRequest(), testObject);
+    test.equal(this.httpCache.getKeyPrefix(),
+      this.httpCache.getVersionPrefix() + this.httpCache.getMobilePrefix());
+    test.done();
+  },
   'should have getResponse method' : function (test) {
     test.expect(2);
     test.isNotNull(this.httpCache.getResponse);
     test.isFunction(this.httpCache.getResponse);
     test.done();
   },
+  'getResponse will be undef if not set' : function (test) {
+    test.expect(2);
+    test.isNotNull(this.httpCache.getResponse);
+    test.isUndefined(this.httpCache.getResponse());
+    test.done();
+  },
   'should have setResponse method' : function (test) {
     test.expect(2);
     test.isNotNull(this.httpCache.setResponse);
     test.isFunction(this.httpCache.setResponse);
+    test.done();
+  },
+  'setResponse should actually set value in object' : function (test) {
+    test.expect(4);
+    test.isNotNull(this.httpCache.getResponse);
+    test.isNotNull(this.httpCache.setResponse);
+    test.isUndefined(this.httpCache.getResponse());
+    var testObject = new Object();
+    this.httpCache.setResponse(testObject);
+    test.equal(this.httpCache.getResponse(), testObject);
     test.done();
   },
   'should have buildKey method' : function (test) {
